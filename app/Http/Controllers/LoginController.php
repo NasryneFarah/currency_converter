e98c56aafb3735cf6b10deba86992578e782532b
Farah KONE <farahyedidiah@gmail.com>
2023-06-23 16:22:29 +0000

    suppression de register

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Http\RedirectResponse;
+// use Illuminate\Http\RedirectResponse;
 
 class LoginController extends Controller
 {
@@ -14,68 +14,36 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-        $user =  new User();
-        $user->name = 'edouard';
-        $user->email = "test@gmail.com";
-        $user->password = Hash::make("test");
-        $user->id_role = 1;
-        $user->save();
-        
-        if (Auth::login($user)) {
+        //je m'assure que mes champs email et password son valide
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        // ma variable  va récupérer que les infos de ces deux champs dans le formulaire
+        $credentials = $request->only('email','password');
+      
+        // La méthode attempt sera utiliser pour gérer les tentatives d'authentification à partir du formulaire de connexion
+
+        if (Auth::attempt($credentials)) {
+            // si l'authentification est réussie
 
             $user = Auth::user();  //la variabe user représente l'utilisateur pour lequel je dois générer un jeton 
-            return response()->json(['User' => $user], 200);
+
+            return response()->json(
+                [
+                    'User' => $user,
+                    'status' => 'success',
+                ], 200);
+            
         } else {
             //si l'authentifiaction a échoué 
-            return response()->json(['message' => 'La connexion a échouée'],401);
+            return response()->json(
+                [
+                 'message' => 'La connexion a échouée'
+                ],401);
         }
-     
-        // //je m'assure que mes champs email et password son valide
-        // $request->validate([
-        //     'email' => 'required|string|email',
-        //     'password' => 'required|string',
-        // ]);
-
-        // // ma variable  va récupérer que les infos de ces deux champs dans le formulaire
-        // $credentials = $request->only('email','password');
-      
-        // // La méthode attempt sera utiliser pour gérer les tentatives d'authentification à partir du formulaire de connexion
-
-        // if (Auth::attempt($credentials)) {
-        //     // si l'authentification est réussie
-
-        //     $user = Auth::user();  //la variabe user représente l'utilisateur pour lequel je dois générer un jeton 
-
-        //     return response()->json(['User' => $user], 200);
-        // } else {
-        //     //si l'authentifiaction a échoué 
-        //     return response()->json(['message' => 'La connexion a échouée'],401);
-        // }
- 
-    }
-
-    public function register(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'role' => $request->id_role
-        ]);
-
-        Auth::login($user);
-
-        return response()->json([
-            'status' => 'success',
-            'message' => 'User created successfully',
-            'user' => $user,
-        ]);
+        
     }
 
     public function logout()
